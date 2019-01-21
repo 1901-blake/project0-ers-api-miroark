a92c4a22d1e163a4eed3beaf7bbd460eaed61903
Michael R. Roark <michaelrroark@gmail.com>
2019-01-21 12:31:00 -0500

    small changes including linting

--- src/models/user/user.roles.ts
+++ src/models/user/user.roles.ts
@@ -2,12 +2,12 @@ export class UserRole {
   id: number; // Primary key
   role: string; // not null; unique
 
-  constructor (id:number, role:string){
+  constructor (id: number, role: string) {
     this.id = id;
     this.role = role;
   }
-};
+}
 
-export const user = new UserRole(0, "user");
-export const manager = new UserRole(1, "Financial MAnager");
-export const admin = new UserRole(2, "Administrator");
+export const user = new UserRole(0, 'user');
+export const manager = new UserRole(1, 'Financial MAnager');
+export const admin = new UserRole(2, 'Administrator');
