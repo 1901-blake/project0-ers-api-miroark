a1ef43ec1d8fb6f3eaf02e2d50a4f027ca0c8d19
Michael R. Roark <michaelrroark@gmail.com>
2019-01-28 15:22:33 -0500

    Added a check to registration to ensure the request has all required information to perform a insert to the database.

--- src/middleware/auth/register.middleware.ts
+++ src/middleware/auth/register.middleware.ts
@@ -4,14 +4,25 @@ import UserDao from '../../daos/user.dao';
 import jwt from 'jsonwebtoken';
 
 export default function registerAuth (req: express.Request, res: express.Response) {
+  // Check that all required fields have been provided.
+  if (!req.body.username ) {
+     res.status(400).send({auth: false, message: 'Missing username.'});
+  } else if (!req.body.password) {
+    res.status(400).send({auth: false, message: 'Missing password.'});
+  } else if (!req.body.email) {
+    res.status(400).send({auth: false, message: 'Missing email.'});
+  } else if (!req.body.firstName) {
+    res.status(400).send({auth: false, message: 'Missing first name.'});
+  } else if (!req.body.lastName) {
+    res.status(400).send({auth: false, message: 'Missing last name.'});
+  }
+
   const hashedPass = bcrypt.hashSync(req.body.password, 8);
   const dao = new UserDao();
 
   dao.insertNewUser(req.body.username, hashedPass, req.body.email,
     req.body.firstName, req.body.lastName)
-    .catch ((err) => {
-      throw err;
-    }).then(returnedIndex => {
+    .then(returnedIndex => {
       dao.getUserbyId(returnedIndex).then( newUser => {
         const secret: any = process.env.JWTSecret;
         const token = jwt.sign({newUser}, secret, {expiresIn: 86400});
@@ -19,5 +30,6 @@ export default function registerAuth (req: express.Request, res: express.Respons
         res.status(200).send({auth: true, token: token, user: newUser});
         }
       ).catch( err => {throw err; });
-    });
+    }).catch ((err) => {throw err; });
+    res.status(500).send({auth: false, message: 'Unknown Error Occured.'});
 }
