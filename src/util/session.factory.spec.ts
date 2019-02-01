c420e4eeabe0e7c93443f09df4c9a19dbb733c92
Michael R. Roark <michaelrroark@gmail.com>
2019-02-01 13:23:44 -0500

    Changed test to output errorcodes.

--- src/util/session.factory.spec.ts
+++ src/util/session.factory.spec.ts
@@ -2,18 +2,22 @@ import SessionFactory from './session.factory';
 
 describe('SessionFactory', () => {
 
-    it('Creates a connection pool.', () => {
+    it('Creates a connection pool.', async () => {
         expect(SessionFactory.created).toBe(false);
         const pool = SessionFactory.getConnectionPool();
-        pool.connect().catch(err => {throw err; });
+        const client = await pool.connect().catch(err => {throw err; });
+        expect(client).toBeDefined;
+        client.release();
         expect(SessionFactory.created).toBe(true);
     });
 
     it('can fetch information from the DB', async () => {
         const pool = SessionFactory.getConnectionPool();
         const client = await pool.connect();
-        const result = await client.query('select * from users').catch(err => {throw err; });
+        const result = await client.query('select * from users')
+        .catch(err => {console.log(err); throw err; });
 
-        expect(result.rows[0]).toBeDefined;
+        const entry = result.rows;
+        expect(entry).toBeDefined;
     });
 });
