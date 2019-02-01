97efa0dc6616a3db99cca21954473b50b9e617a0
Michael R. Roark <michaelrroark@gmail.com>
2019-02-01 11:35:02 -0500

    Updated tests to test Database Connectivity.

---
+++ src/util/session.factory.spec.ts
@@ -0,0 +1,19 @@
+import SessionFactory from './session.factory';
+
+describe('SessionFactory', () => {
+
+    it('Creates a connection pool.', () => {
+        expect(SessionFactory.created).toBe(false);
+        const pool = SessionFactory.getConnectionPool();
+        pool.connect().catch(err => {throw err; });
+        expect(SessionFactory.created).toBe(true);
+    });
+
+    it('can fetch information from the DB', async () => {
+        const pool = SessionFactory.getConnectionPool();
+        const client = await pool.connect();
+        const result = await client.query('select * from users').catch(err => {throw err; });
+
+        expect(result).toBeDefined;
+    });
+});
