a0d1752fe72043e4ee4aca9fd3ff7988b5bd5358
Michael R. Roark <michaelrroark@gmail.com>
2019-02-08 15:17:48 -0500

    Added x-access-token to cors

--- src/middleware/auth/access.control.ts
+++ src/middleware/auth/access.control.ts
@@ -4,7 +4,7 @@ export default async function accessControl (req: express.Request, res: express.
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', String(req.headers.origin));
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');    if ('OPTIONS' == req.method) {
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-access-token');    if ('OPTIONS' == req.method) {
         res.sendStatus(200);
     } else {
         next();
